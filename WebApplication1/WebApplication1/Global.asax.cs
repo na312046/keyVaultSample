a2dff30814b0cd15db541d7603767260508bea3a
Ubuntu <devopsadmin@linuxHost.linuxHost-716n77k0.j8.internal.cloudapp.net>
2017-02-24 16:51:13 +0000

    initial commit

---
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.KeyVault;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebApplication1.Util;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(KeyVaultHelper.GetToken));
+            var task = kv.GetSecretAsync(WebConfigurationManager.AppSettings["connectionString"]);
+            task.Wait();
+            KeyVaultHelper.connectionStringSecret = task.Result.Value;
+        }
+    }
+}
